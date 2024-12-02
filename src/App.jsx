08d3501431c5f7b637dd585a0200ee3c12f5a0a5
Pujitha <pujithadangeti@gmail.com>
2024-12-02 19:42:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './assets/Components/Layout/Header'
+import Main from './assets/Components/Layout/Main'
+import Footer from './assets/Components/Layout/footer'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
