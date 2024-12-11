0aace424e05ccd7240f8d90e84d06c867f6c44d7
BhaskaraVarma <alluribhaskar007@gmail.com>
2024-12-11 18:20:14 +0530

    My Contribution on Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './assets/Components/Layout/Header'
-import Main from './assets/Components/Layout/Main'
-import Footer from './assets/Components/Layout/footer'
+// import './App.css'
+
+
+import AppRouters from './Routers/AppRouters';
+import MainLayout from './Layouts/MainLayout'
+import Main from './assets/Components/Main';
+
+
 
 function App() {
 
 
   return (
-    <>
     
-      <Header/>
-      <Main/>
-      <Footer/>
-    </>
+    <>
+      <AppRouters/>
+     
+      </>
+      
+      
+    
   )
 }
 
